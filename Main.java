5416ab65c2665c06b136ead866113c879b47e122
Cristosic <138037967+Cristosic@users.noreply.github.com>
2023-07-23 18:35:31 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        if (check(101,99)) System.out.println("The equipment works fine");
+        else System.out.println("The equipment doesn't work fine");
+
+    }
+    public static boolean check(int temperature1, int temperature2){
+        if (temperature1>100 && temperature2<100) return true;
+        else return false;
+    }
+}
